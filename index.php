38b842e1fa2b9f9a84ab936617d6e3462fcf7062
Gerta431 <musligerta08@gmail.com>
2025-03-17 17:44:37 +0100

    pushing again

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                                 <div class="meta-info">
                                     <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
                                     <p>Categories: <?php the_category(''); ?> </p>
-                                    <p>Tags: <?php the_tags('','',); ?></p>
+                                    <p>Tags: <?php the_tags('', '',); ?></p>
                                 </div>
                                 <?php the_content(); ?>
                             </article>
