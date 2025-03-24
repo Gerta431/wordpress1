1029f61ec4b643e50797403f8ecd80686f8cafcc
Gerta431 <musligerta08@gmail.com>
2025-03-24 17:47:06 +0100

    push

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,12 +1,11 @@
-console ("testing")
+let dropdown = document.querySelector('.menu'), //ul
+submenu = document.querySelector('.sub-menu'), //ul li a
+buttonClick = document.querySelector('.check-button'), //button
+hamburger = document.querySelector('.menu-icon');
 
-let dropdown = document.querySelector('.menu'),//ul
-submenu = document.querySelector('sub-menu'),//ul li a
-buttonClick = document.querySelector('.menu-icon');
-
-buttonClick.addEventListener('click',() =>{
+buttonClick.addEventListener( 'click', () => {
     dropdown.classList.toggle('show-dropdown');
-    if(submenu){
+    if( submenu ){
         submenu.classList.toggle('show-dropdown');
     }
     hamburger.classList.toggle('animate-button');
